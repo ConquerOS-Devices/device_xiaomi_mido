990af233f48402ec0cd01d5e7f6019d5b5e7011f
ikeh045 <njkampret@gmail.com>
2021-07-23 06:04:24 +0700

    mido: aex ify

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -22,19 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common PixelPlusUI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# FaceUnlock & Gapps
-TARGET_FACE_UNLOCK_SUPPORTED = true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-
-# Some Extra's
-CUSTOM_BUILD_TYPE=OFFICIAL
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
